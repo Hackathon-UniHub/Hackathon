f32a786e528143a43560d6364128982fda0f684e
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-06-06 22:15:04 -0300

    feat: adicionado o vue; esqueleto do header

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <p>&copy; 2026 UniHub - Todos os direitos reservados.</p>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <RouterLink><img src="../icons/UniHub.png" alt="Logo UniHub" /></RouterLink>
+      </div>
+      <nav>
+        <ul>
+          <li><RouterLink>Home</RouterLink></li>
+          <li><RouterLink>Favoritos</RouterLink></li>
+          <li><RouterLink>Sobre Nós</RouterLink></li>
+          <li><span class="nav-border"><RouterLink>Explore pelo Mapa!</RouterLink></span></li>
+        </ul>
+        <div class="login">
+          <ul>
+            <li><RouterLink>Entrar</RouterLink></li>
+            <li><span class="login-border"><RouterLink>Criar Conta</RouterLink></span></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    background-color: white;
+  }
+  header .container {
+    display: flex;
+    justify-content: space-between;
+  }
+  header .container nav {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
