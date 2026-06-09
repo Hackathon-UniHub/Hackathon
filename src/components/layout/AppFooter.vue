f4901b0779a644edd98c553b685393b6357b4a26
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-06-09 09:34:10 -0300

    feat: header feito; Partindo pro footer

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,8 +2,42 @@
 
 <template>
   <footer>
-    <p>&copy; 2026 UniHub - Todos os direitos reservados.</p>
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <RouterLink><img src="" alt="Instagram" /></RouterLink>
+          </li>
+          <li>
+            <RouterLink><img src="" alt="X" /></RouterLink>
+          </li>
+          <li>
+            <RouterLink><img src="" alt="TikTok" /></RouterLink>
+          </li>
+        </ul>
+      </nav>
+      <div class="contato">
+        <h2>Nos contate para mais informaões!</h2>
+        <p>+55 47 99708-5431</p>
+      </div>
+      <div class="copyright">
+        <p>&copy; 2026 UniHub - Todos os direitos reservados.</p>
+      </div>
+    </div>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  background-color: black;
+}
+.container nav {
+  text-align: center;
+}
+.container nav ul {
+  display: flex;
+}
+a {
+  text-decoration: none;
+}
+</style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -8,34 +8,85 @@ import { RouterLink } from 'vue-router'
       <div class="logo">
         <RouterLink><img src="../icons/UniHub.png" alt="Logo UniHub" /></RouterLink>
       </div>
+
       <nav>
         <ul>
           <li><RouterLink>Home</RouterLink></li>
           <li><RouterLink>Favoritos</RouterLink></li>
           <li><RouterLink>Sobre Nós</RouterLink></li>
-          <li><span class="nav-border"><RouterLink>Explore pelo Mapa!</RouterLink></span></li>
+          <li>
+            <span class="nav-border"><RouterLink>Explore pelo Mapa!</RouterLink></span>
+          </li>
         </ul>
-        <div class="login">
-          <ul>
-            <li><RouterLink>Entrar</RouterLink></li>
-            <li><span class="login-border"><RouterLink>Criar Conta</RouterLink></span></li>
-          </ul>
-        </div>
       </nav>
+
+      <div class="login">
+        <ul>
+          <li><RouterLink>Entrar</RouterLink></li>
+          <li>
+            <span class="login-border"><RouterLink>Criar Conta</RouterLink></span>
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped>
-  header {
-    background-color: white;
-  }
-  header .container {
-    display: flex;
-    justify-content: space-between;
-  }
-  header .container nav {
-    display: flex;
-    justify-content: space-between;
-  }
+header {
+  background-color: white;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1px 2vw;
+}
+nav ul {
+  display: flex;
+  align-items: center;
+  gap: 2.5vw;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+nav ul li {
+  font-size: 16px;
+  font-weight: 500;
+}
+.login ul {
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.login ul li {
+  font-size: 16px;
+  font-weight: 500;
+}
+.nav-border,
+.login-border {
+  background-color: #e4c97d;
+  padding: 5px 1vw;
+  border-radius: 10px;
+  display: inline-block;
+}
+.nav-border:hover,
+.login-border:hover {
+  cursor: pointer;
+  transition: background-color 0.2s;
+  background-color: #e4c369;
+}
+a {
+  text-decoration: none;
+  color: #000;
+  font-size: 1.1rem;
+}
+a:hover {
+  font-weight: 500;
+  transition: font-weight 0.1s;
+  cursor: pointer;
+}
 </style>
