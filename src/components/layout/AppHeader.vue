cd23717d6740c95bdf2f9705380edfe0c1d8d627
Matheus-MxLa <matheuslacerdamaximiano@gmail.com>
2026-08-03 15:40:19 -0300

    adicionei a pagina filtro no header

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -12,6 +12,7 @@ import { RouterLink } from 'vue-router'
       <nav>
         <ul>
           <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/universidadesfiltro">Filtro</RouterLink></li>
           <li><RouterLink to="#">Favoritos</RouterLink></li>
           <li><RouterLink to="#">Sobre Nós</RouterLink></li>
           <li>
