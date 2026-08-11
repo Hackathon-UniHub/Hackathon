1760b2fa049ef41bb5c20cbdf2d0bc43398b94b4
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-08-11 09:26:41 -0300

    feat: configurado o css global e adicionado algumas coisas no AppHeader para poder ver se a aplicação deu certo

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,20 @@
+<!-- filepath: /home/tempz/Documentos/Hackathon/src/components/layout/AppHeader.vue -->
 <template>
-  <header>
-    <nav>
-      <a href="/">Home</a>
-    </nav>
+  <header class="header">
+    <div class="container">
+      <a class="brand" href="/">
+        <span class="brand-mark">H</span>
+        <span class="brand-name">Hackathon</span>
+      </a>
+
+      <nav class="nav">
+        <a href="/">Home</a>
+        <a href="/about">Sobre</a>
+        <a href="/services">Serviços</a>
+        <a href="/contact">Contato</a>
+      </nav>
+
+      <a class="btn" href="/contact">Fale conosco</a>
+    </div>
   </header>
 </template>
