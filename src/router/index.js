a1f35fb67fb2a21cc44fb66b429c06ea457fa55d
tempz-miguelAndrade <miguelandradedesouzaifc@gmail.com>
2026-08-27 19:03:52 -0300

    feat: ajeitando erro no header e no index .js para poder fazer o pull request pra dev

--- src/router/index.js
+++ src/router/index.js
@@ -4,33 +4,23 @@ import HomeView from '../views/HomeView.vue'
 import paginaFiltroList from '@/components/paginaFiltro/paginaFiltroList.vue'
 import paginaUniversidade from '@/components/paginaUniversidades/paginaUniversidade.vue'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/universidades',
-      name: 'filtro',
-      component: paginaFiltroList,
-    },
-    {
-      path: '/universidade/:id',
-      name: 'universidade',
-      component: paginaUniversidade,
-    },
-  ],
-  component: () => import('../views/AboutView.vue'),
 //import { useAuthStore } from '@/stores/auth'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/HomeView.vue'),
+    component: HomeView,
+  },
+  {
+    path: '/universidades',
+    name: 'filtro',
+    component: paginaFiltroList,
+  },
+  {
+    path: '/universidade/:id',
+    name: 'universidade',
+    component: paginaUniversidade,
   },
   {
     path: '/login',
